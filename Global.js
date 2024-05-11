aadc235892a6277f4521e80f9b3745dd511e3f98
Tanishq Rathi <164673615+TanishqRathi12@users.noreply.github.com>
2024-05-11 14:20:58 +0530

    project added

---
+++ Global.js
@@ -0,0 +1,37 @@
+const main = document.getElementById('article-cont4');
+const article = document.createElement('article');
+
+let data;
+let datalength = 0;
+async function getData(){
+    const result = await fetch("https://saurav.tech/NewsAPI/everything/cnn.json");
+    const res = await result.json();
+    data = res.articles;
+    datalength = data.length;
+    console.log(data);
+    renderUI();
+}   
+getData();
+
+function renderUI(){
+    for(let i=0;i<datalength;i++){
+        const article = document.createElement('article');
+        article.innerHTML=`
+        <a href="${data[i].url}" target=_blank style="color: rgb(0, 0, 0);">
+        <img class="Article-img" src="${data[i].urlToImage}" alt="Article image">     
+        <div class="article-content" >
+            <h2>
+               ${data[i].title}
+            </h2>
+            <p>
+                ${data[i].description}
+            </p>
+            
+        </a>
+        </div>
+        `;
+        main.appendChild(article);
+       
+    }
+        
+}
